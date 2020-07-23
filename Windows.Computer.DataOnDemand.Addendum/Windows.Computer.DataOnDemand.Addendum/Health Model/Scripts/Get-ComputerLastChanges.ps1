bc42f1d4642a9bd35c27131598bf8c8eba31bfab
Ruben Zimmermann <rubenz@gmx.de>
2020-07-23 21:31:15 +0200

    Updated to version 166
    Added MS-Graph support

---
+++ Windows.Computer.DataOnDemand.Addendum/Windows.Computer.DataOnDemand.Addendum/Health Model/Scripts/Get-ComputerLastChanges.ps1
@@ -0,0 +1,133 @@
+﻿<#
+.SYNOPSIS
+    Get Compupter Last Changes
+.DESCRIPTION
+    This script gets some last changes such as last installed SW or logged on user.
+.Notes
+	AUTHOR: Ruben Zimmermann @ruben8z
+	LASTEDIT: 2019-12-25
+	REQUIRES: PowerShell Version 2, Windows Management Foundation 4, At least Windows 7 or Windows Server 2008 R2.	
+REMARK:
+This PS script comes with ABSOLUTELY NO WARRANTY; for details see gnu-gpl. This is free software, and you are welcome to redistribute it under certain conditions; see gnu-gpl for details.
+    
+#>
+Param(
+    [ValidateSet("text","csv","json", "list")]
+    [string] $Format = "csv"
+)
+
+
+$ErrorActionPreference = "stop"
+$lastInstallFlag       = 'Fine'
+
+$regPat         = '[0-9]{8}'
+$bootInfo       = wmic os get lastbootuptime
+$bootDateNumber = Select-String -InputObject $bootInfo -Pattern $regPat | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
+$bootDate       = ([DateTime]::ParseExact($bootDateNumber,'yyyyMMdd',[Globalization.CultureInfo]::InvariantCulture))
+$lastBootTime   = $bootDate | Get-Date -Format 'yyyy-MM-dd'
+
+$timeSpan       = New-TimeSpan -Start $lastBootTime -End (Get-Date)
+$UpTimeInDays   = [math]::Round($timeSpan.TotalDays,1)
+
+try {
+	$soft32All       = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.Publisher -notlike "*Microsoft*" } | Select-Object DisplayName, Publisher, InstallDate
+	$soft32Filtered  = $soft32All | Select-Object DisplayName, Publisher, @{Name='RealDate';Expression={([DateTime]::ParseExact($_.InstallDate,'yyyyMMdd',[Globalization.CultureInfo]::InvariantCulture)) `
+							  | Get-Date -Format 'yyyy-MM-dd'}}   
+
+	$lastInstalled32Sofware             = $soft32Filtered | Sort-Object -Property RealDate -Descending | Select-Object -First 1
+	$lastInstalled32SoftwareInstallDate = $lastInstalled32Sofware.RealDate | Get-Date -Format 'yyyy-MM-dd'
+	$lastInstalled32SoftwareName        = $lastInstalled32Sofware.DisplayName
+} catch {
+	$lastInstalled32SoftwareInstallDate = 'Not Available'
+	$lastInstalled32SoftwareName        = 'Not Available'
+	$lastInstallFlag                     = 'Error'
+}
+  
+try {
+	$soft64All       = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.Publisher -notlike "*Microsoft*" } | `
+										Select-Object DisplayName, Publisher, InstallDate
+
+	$soft64Filtered  = $soft64All | Select-Object DisplayName, Publisher, @{Name='RealDate';Expression={([DateTime]::ParseExact($_.InstallDate,'yyyyMMdd',[Globalization.CultureInfo]::InvariantCulture)) `
+								  | Get-Date -Format 'yyyy-MM-dd'}}   
+
+	$lastInstalled64Sofware              = $soft64Filtered | Sort-Object -Property RealDate -Descending | Select-Object -First 1
+	$lastInstalled64SoftwareInstallDate  = $lastInstalled64Sofware.RealDate | Get-Date -Format 'yyyy-MM-dd'
+	$lastInstalled64SoftwareName         = $lastInstalled64Sofware.DisplayName
+} catch{
+	$lastInstalled64SoftwareInstallDate  = 'Not Available'
+	$lastInstalled64SoftwareName         = 'Not Available'
+	$lastInstallFlag                     = 'Error'
+}
+
+if ($lastInstallFlag -eq 'Fine')  {
+	if ($lastInstalled32SoftwareInstallDate -gt $lastInstalled64SoftwareInstallDate) {
+		$lastInstalledSoftwareInstallDate = $lastInstalled32SoftwareInstallDate
+		$lastInstalledSoftwareName        = $lastInstalled32SoftwareName		
+	} else {
+		$lastInstalledSoftwareInstallDate = $lastInstalled64SoftwareInstallDate
+		$lastInstalledSoftwareName        = $lastInstalled64SoftwareName		
+	}		
+} else {
+	$lastInstalledSoftwareInstallDate = 'Not Available'
+	$lastInstalledSoftwareName        = 'Not Available'
+}
+
+try {
+	$regPat                         = 'KB[0-9]{7}'
+	$Session                        = New-Object -ComObject "Microsoft.Update.Session"
+	$Searcher                       = $Session.CreateUpdateSearcher()
+	$historyCount                   = $Searcher.GetTotalHistoryCount()
+	$allHotfixes                    = $Searcher.QueryHistory(0, $historyCount) | Select-Object Date, @{Name='KBNo';Expression={(Select-String -InputObject $_.Title -Pattern $regPat | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value)}}
+	$lastHotfix                     = $allHotfixes | Sort-Object -Descending -Property Date | Sort-Object -Descending -Property KBNo | Select-Object -First 1
+
+	$lastInstalledHotfixInstallDate = $lastHotfix.Date | Get-Date -Format 'yyyy-MM-dd'
+	$lastInstalledHotfixName        = $lastHotfix.KBNo
+} catch{
+	$lastInstalledHotfixInstallDate = 'Not Available'
+	$lastInstalledHotfixName        = 'Not Available'
+}
+
+
+try {
+	$noOfDaysDiff = (New-TimeSpan -Start $lastBootTime -End $lastInstalledHotfixInstallDate).Days
+	if($noOfDaysDiff -gt 1) {
+		$patchBootPending = "Yes, for $noOfDaysDiff Days"
+	} else {
+		$patchBootPending = "No."
+	}
+} catch {
+	$patchBootPending = 'Not Available'
+}
+		
+
+try {
+	$profilesDir          = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | Select-Object -ExpandProperty ProfilesDirectory
+	$lastLoggedOnInfo     = Get-ChildItem -Path $profilesDir | Select-Object Name, LastWriteTime | Sort-Object -Property LastwriteTime -Descending | Select-Object -First 1
+	$lastLoggedOnUserId   = $($lastLoggedOnInfo.Name).ToUpper()
+	$lastLoggedOnUserDate = $lastLoggedOnInfo.LastWriteTime | Get-Date -Format 'yyyy-MM-dd'
+} catch{
+	$lastLoggedOnUserId   = 'Not Available'
+	$lastLoggedOnUserDate = 'Not Available'
+}
+
+
+$myComputerDescHash = @{'Last Boot / UpTime (days)' = $($lastBootTime + ' / ' + $UpTimeInDays)}
+$myComputerDescHash.Add('Last Interactive Logon', $($lastLoggedOnUserId + ' / ' + $lastLoggedOnUserDate))
+$myComputerDescHash.Add('Last Software Install', $($lastInstalledSoftwareName + ' / ' + $lastInstalledSoftwareInstallDate))
+$myComputerDescHash.Add('Last Hotfix Install', $($lastInstalledHotfixName + ' / ' + $lastInstalledHotfixInstallDate))
+$myComputerDescHash.Add('Patch-Boot pending', $($patchBootPending))
+
+$myComputerDescObj = New-Object -TypeName PSObject -Property $myComputerDescHash
+
+
+if ($Format -eq 'text') {
+    $myComputerDescObj | Format-Table -AutoSize | Out-String -Width 4096 | Write-Host
+} elseif ($Format -eq 'csv') {
+    $myComputerDescObj | ConvertTo-Csv -NoTypeInformation | Out-String -Width 4096 | Write-Host
+} elseif ($Format -eq 'json') {
+    $myComputerDescObj | ConvertTo-Json | Out-String -Width 4096 | Write-Host
+} elseif ($format -eq 'list') {
+    $myComputerDescObj | Format-List | Out-String -Width 4096 | Write-Host
+}
+
+# Done. (do not remove blank line following this comment as it can cause problems when script is sent to SCOM agent!)
---
+++ Windows.Computer.DataOnDemand.Addendum/Windows.Computer.DataOnDemand.Addendum/Health Model/Scripts/Get-MSGraphData.ps1
@@ -0,0 +1,250 @@
+﻿<#
+.SYNOPSIS
+	Retrieves information from MS Graph / MS Graph Beta
+.DESCRIPTION
+	Retrieves information from MS Graph / MS Graph Beta and returns JSON or CSV
+.Notes
+	AUTHOR: Ruben Zimmermann @ruben8z
+	LASTEDIT: 2020-07-11
+	REQUIRES: PowerShell Version 2, Windows Management Foundation 4, At least Windows 7 or Windows Server 2008 R2.	
+REMARK:
+This PS script comes with ABSOLUTELY NO WARRANTY; for details see gnu-gpl. This is free software, and you are welcome to redistribute it under certain conditions; see gnu-gpl for details.
+	
+#>
+Param(
+	[ValidateSet('text','list','json')]
+	[string]$Format = "json",
+	[string]$WebServiceUrl = '',
+	[string]$GraphQry = '',
+	[string]$ClientId = '',
+	[string]$TenantId = '',
+	[string]$ClientSecret = '',	
+	[string]$SortedBy = '',
+	[string]$FilteredBy = '',
+	[string]$addVisualizationFlag = 'true',	
+	[string]$SortDescending = '',	
+	[string]$DisplayItemNumber = ''	
+)
+
+#$dbgFile = 'C:\Temp\graph.log.txt'
+
+$api = New-Object -ComObject 'MOM.ScriptAPI'
+
+$ErrorActionPreference = "stop"
+$rtnMsg = ''
+
+if ($WebServiceUrl -match "(?i)http(s)?") {
+	$foo = 'proceed'
+} else {
+	$rtnMsg = 'WebService not matching URL' + $WebServiceUrl
+}
+
+if ([guid]::TryParse($ClientId, $([ref][guid]::Empty))) {
+	$foo = 'proceed'
+} else {
+	$rtnMsg = 'ClientId not matching GUID' + $ClientId
+}
+
+if ([guid]::TryParse($TenantId, $([ref][guid]::Empty))) {
+	$foo = 'proceed'
+} else {
+	$rtnMsg = 'TenantId not matching GUID' + $TenantId
+}
+
+if ($ClientSecret.Length -gt 11) {
+	$foo = 'proceed'
+} else {
+	$rtnMsg = 'ClientSecret too short. Use at least 12 characters! ' + $ClientSecret
+}
+
+if ($FilteredBy -match "{}") {
+	$FilteredBy = $FilteredBy -replace ('{|}','')
+} else {
+	$foo = 'bar'
+}
+
+if ($GraphQry.Length -gt 11) {
+	$foo = 'proceed'
+} else {
+	$rtnMsg = 'GraphQry appears to be invalid. Too short.' +  $GraphQry
+}
+
+if ($GraphQry.Substring(0,1) -eq "/") {
+	$foo = 'proceed'
+} else {
+	$GraphQry = '/' +  $GraphQry
+}
+
+if ($GraphQry -match '&amp;') {
+	$GraphQry = $GraphQry -replace '&amp;','&'    
+	$GraphQry = $GraphQry -replace '`',''    
+}
+
+if ($DisplayItemNumber -match '\d') {
+	$foo = 'bar'
+} else {
+	$rtnMsg = 'DisplayItemNumber is not a number. Invalid.' +  $DisplayItemNumber
+}
+
+if ($SortDescending -match '(?i)true|false|$true|$false') {
+	$foo = 'bar'
+} else {
+	$rtnMsg = 'SortDescindg value is invalid.' +  $SortDescending
+}
+
+
+$api.LogScriptEvent('Get-MSGraphData.ps1',602,1,"URL $($WebServiceUrl), TenantID $($TenantId) ClientID $($ClientId) FilteredBy $($FilteredBy) Sortedby $($SortedBy) GraphQry $($GraphQry)")
+
+# Construct URI
+$uri = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
+
+# Construct Body
+$body = @{
+	client_id     = $ClientId
+	scope         = "https://graph.microsoft.com/.default"
+	client_secret = $ClientSecret
+	grant_type    = "client_credentials"
+}
+
+# Get OAuth 2.0 Token
+$tokenRequest = Invoke-WebRequest -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing
+
+# Access Token
+$token  = ($tokenRequest.Content | ConvertFrom-Json).access_token
+
+$uri = $WebServiceUrl + $GraphQry	
+
+#"165"| Out-File -FilePath $dbgFile -Append
+#$uri  | Out-File -FilePath $dbgFile -Append
+
+$api.LogScriptEvent('Get-MSGraphData.ps1',602,2,"Qury URL: $($uri)")
+
+try {	
+	$query = Invoke-RestMethod -Method Get -Uri $uri -ContentType "application/json" -Headers @{Authorization = "Bearer $token"} -ErrorAction Stop -UseBasicParsing 
+} catch {	
+	$rtnMsg  = 'Failure during InvokeWebRequest  ' + $Error
+	$rtnMsg += 'URI: ' + $uri 
+}
+
+#$query  | Out-File -FilePath $dbgFile -Append
+
+$api.LogScriptEvent('Get-MSGraphData.ps1',606,1,"rtnMsg $($rtnMsg)")
+
+$allElements = New-Object -TypeName 'System.Collections.Generic.List[PSObject]'
+
+$elementCount = 0
+$elementCount = $query.value.count
+
+$query.value  | ForEach-Object { $allElements.Add($_) }
+
+$api.LogScriptEvent('Get-MSGraphData.ps1',602,1,"Answ Value $($query.value.count) ")
+
+if ($elementCount -gt 1) {
+
+	if ($FilteredBy -ne '') {
+		if ($FilteredBy -match '(?i)[a-zA-Z$_\.''" -]{1,}\s{1,}-(ieq|ine|eq|ne|gt|ge|lt|le|or|and|like|notlike|match|notmatch)\s{1,}[a-zA-Z$_\.''" -]{1,}')  {
+			$FilterFor = $ExecutionContext.InvokeCommand.NewScriptBlock($FilteredBy)				
+			$allElements = $allElements | Where-Object -FilterScript $FilterFor
+			$api.LogScriptEvent('Get-MSGraphData.ps1',603,2,"regex passed for $FilteredBy")
+		} else {
+			$api.LogScriptEvent('Get-MSGraphData.ps1',603,2,"regex NOT passed for $FilteredBy")
+		}
+	}
+
+	if ($SortedBy -match '(?i)\w{1,}') {
+		$allElements = $allElements | Sort-Object -Property $SortedBy 	
+		if ($SortDescending -match '(?i)true|false|$true|$false') {
+			if ($SortDescending -match '(?i)true|$true') {
+				$allElements = $allElements | Sort-Object -Property $SortedBy -Descending
+			} 
+		}
+	}
+
+	if ($DisplayItemNumber -match '\d' -and $DisplayItemNumber -gt 0) {
+		$allElements = $allElements | Select-Object -First $DisplayItemNumber
+	}	
+
+	if ($addVisualizationFlag -ieq 'True') {	
+
+		$allElementsVis = New-Object -TypeName 'System.Collections.Generic.List[psobject]'
+	
+		for ($i=0; $i -lt $allElements.Count; $i++) {
+
+			if ($i % 2 -eq 0) {
+				$itmElementFlagHash = @{'Counter' = "## $($i)"}
+				$itmElementFlagHash.Add('TotalNumber', $elementCount)
+				$tmpObject = $allElements[$i] 
+				$objMembers = $tmpObject.psobject.Members | Where-Object {$_.MemberType -eq 'NoteProperty'}
+				for ($j = 0; $j -lt $objMembers.Count; $j++) {
+					$itmElementFlagHash.Add($($objMembers[$j].Name), "## $($objMembers[$j].Value)")
+				}            
+			} else {
+				$itmElementFlagHash = @{'Counter' = "$($i)"}      
+				$itmElementFlagHash.Add('TotalNumber', $elementCount)
+				$tmpObject = $allElements[$i] 
+				$objMembers = $tmpObject.psobject.Members | Where-Object {$_.MemberType -eq 'NoteProperty'}
+				for ($j = 0; $j -lt $objMembers.Count; $j++) {
+					$itmElementFlagHash.Add($($objMembers[$j].Name), "$($objMembers[$j].Value)")
+				}
+			}
+		
+			$itmElementFlagObj = New-Object -TypeName PSObject -Property $itmElementFlagHash
+			$allElementsVis.Add($itmElementFlagObj)
+
+		} #end for ($i=0; $i -lt $allElements.Count; $i++) {}
+
+		$allElements = $null
+		$allElements = $allElementsVis
+
+	} #end if ($addVisualizationFlag -ieq 'True')
+
+} else {
+
+	$allElementsNoVis = New-Object -TypeName 'System.Collections.Generic.List[psobject]'
+
+	for ($i=0; $i -lt $allElements.Count; $i++) {
+
+			
+		$itmElementFlagHash = @{'Counter' = "## $($i)"}           
+		$tmpObject = $allElements[$i] 
+		$objMembers = $tmpObject.psobject.Members | Where-Object {$_.MemberType -eq 'NoteProperty'}
+		for ($j = 0; $j -lt $objMembers.Count; $j++) {
+			$itmElementFlagHash.Add($($objMembers[$j].Name), "## $($objMembers[$j].Value)")
+		}        
+		
+		$itmElementFlagHash.Add('TotalNumber', $elementCount)
+		$itmElementFlagObj = New-Object -TypeName PSObject -Property $itmElementFlagHash
+		$allElementsNoVis.Add($itmElementFlagObj)
+
+	} #end for ($i=0; $i -lt $allElements.Count; $i++) {}
+
+	$allElements = $null
+	$allElements = $allElementsNoVis
+
+} #end  ($query.value.count -gt 1) 
+
+
+
+if ($Format -ieq 'text') {
+	$allElements | Format-Table -AutoSize | Out-String -Width 4096 | Write-Host
+} elseif ($Format -ieq 'json') {
+	$allElements | ConvertTo-Json | Out-String -Width 4096 | Write-Host
+} elseif ($format -ieq 'list') {
+	$allElements | Format-List | Out-String -Width 4096 | Write-Host
+}
+
+if ($Error) {
+	$rtnMsg += " error count: $($Error.Count) "
+	$rtnMsg += " --- "
+	if ($Error.Count -gt 0) {
+		for ($l = 0; $l -lt $($Error.count); $l++ ) {
+			$rtnMsg += " Error No $l $($Error[$l]) " 
+		}
+	}
+	$api.LogScriptEvent('Get-MSGraphData.ps1',607,1,$rtnMsg)
+} else {
+	$api.LogScriptEvent('Get-MSGraphData.ps1',607,4,"no error!")
+}
+
+
+# Done. (do not remove blank line following this comment as it can cause problems when script is sent to SCOM agent!)
